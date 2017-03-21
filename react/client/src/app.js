1161e617e914a244e9f21e18716493aa42205846
Andrewrlonsdale <Andrewrlonsdale@hotmail.com>
2017-03-21 11:21:51 +0000

    add logic to app.js

---
+++ react/client/src/app.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Main from './containers/Main'
+import Home from './containers/Home'
+import Assignments from './containers/Assignments'
+
+import Login from './containers/Login'
+import {Router, Route, IndexRoute, hashHistory} from 'react-router'
+
+class App extends React.Component{
+
+  render(){
+    return (
+      <Router history={hashHistory}>
+        <Route path='/' component={Main}>
+          <IndexRoute component={Home} />
+          <Route path='/login' component={Login} />
+          <Route path='/assignments' component={Assignments} />
+          
+        </Route>
+      </Router>
+    )
+  }
+}
+
+
+ReactDOM.render(<App />, document.getElementById('app'))
