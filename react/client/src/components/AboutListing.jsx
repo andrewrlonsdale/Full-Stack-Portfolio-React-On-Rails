9fb3da50561952a6c9ee1822b40769a1a5679266
Andrewrlonsdale <Andrewrlonsdale@hotmail.com>
2017-03-21 11:34:43 +0000

    add logic to AboutListing.jsx

---
+++ react/client/src/components/AboutListing.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Link, browserHistory } from 'react-router'
+
+const aboutListing = function (props) {
+
+    return (
+      <div className='assignment-listing'>
+          <h2>hello </h2>
+      </div>
+  )
+
+}
+
+const { string, text } = React.PropTypes
+
+export default aboutListing
+
